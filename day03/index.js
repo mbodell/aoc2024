6035671cb1138e6cf4f7ea9006817d467ddf8c3e
Michael Bodell <mbodell@gmail.com>
2024-12-03 22:10:33 -0800

    Solved part 1

--- day03/index.js
+++ day03/index.js
@@ -6,8 +6,12 @@ const eachLine = Promise.promisify(lineReader.eachLine);
 let filename = process.argv.slice(2)[0] || 'input.txt';
 
 let answer = 0;
+let terms = [];
 
 eachLine(filename, function(line) {
+	let term = line.match(/mul\(\d{1,3},\d{1,3}\)/g);
+	terms.push(term);
+	answer += term.map((x)=>x.match(/\d+/g).reduce((a,b)=>parseInt(a)*parseInt(b))).reduce((a,b)=>a+b);
 }).then(function(err) {
   console.log(answer);
 });
