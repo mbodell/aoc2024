b11a447dc89a16330fa8d732619eba564c4f0e6d
Michael Bodell <mbodell@gmail.com>
2024-12-17 23:24:37 -0800

    Solved part1

--- day18/index.js
+++ day18/index.js
@@ -7,18 +7,67 @@ let filename = process.argv.slice(2)[0] || 'input.txt';
 
 let answer = 0;
 
+/* example
 const GRID=7;
 const HIT=12;
+*/
+const GRID=71;
+const HIT=1024;
 
 let inp=[];
 let map=[];
+let visit=[];
+
+function solvePath() {
+	let paths = [];
+	paths.push([0,[0,0],[]]);
+	let solve = false;
+	while(!solve) {
+		let cur = paths.shift();
+		let y = cur[1][0];
+		let x = cur[1][1];
+		if((y===(GRID-1))&&(x===(GRID-1))) {
+			solve = true;
+			answer = cur[0];
+		} else if(visit[y][x]===0) {
+			visit[y][x] = 1;
+			if(y>0) {
+				let np = visit[y-1][x];
+				if(np===0 && map[y-1][x]!=='#') {
+					paths.push([cur[0]+1,[y-1,x],cur[2].concat([[y,x]])]);
+				}
+			}
+			if(y+1<GRID) {
+				let np = visit[y+1][x];
+				if(np===0 && map[y+1][x]!=='#') {
+					paths.push([cur[0]+1,[y+1,x],cur[2].concat([[y,x]])]);
+				}
+			}
+			if(x>0) {
+				let np = visit[y][x-1];
+				if(np===0 && map[y][x-1]!=='#') {
+					paths.push([cur[0]+1,[y,x-1],cur[2].concat([[y,x]])]);
+				}
+			}
+			if(x+1<GRID) {
+				let np = visit[y][x+1];
+				if(np===0 && map[y][x+1]!=='#') {
+					paths.push([cur[0]+1,[y,x+1],cur[2].concat([[y,x]])]);
+				}
+			}
+		}
+	}
+}
 
 eachLine(filename, function(line) {
 	inp.push(line.split(',').map((x)=>parseInt(x)));
 }).then(function(err) {
 	for(let y=0; y<GRID; y++) {
+		map[y] = [];
+		visit[y] = [];
 		for(let x=0; x<GRID; x++) {
 			map[y][x] = '.';
+			visit[y][x] = 0;
 		}
 	}
 	for(let i=0;i<HIT;i++) {
